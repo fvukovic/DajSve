12c7386be73de90939380f6fbfc0ce6fdbcef54d
hkisicek <hkisicek@foi.hr>
2017-01-31 22:58:47 +0100

    Automatizirani test (Espresso)
    Automatizirani test za ponude

--- DajSveApp/app/src/androidTest/java/hr/foi/air/dajsve/ExampleInstrumentedTest.java
+++ DajSveApp/app/src/androidTest/java/hr/foi/air/dajsve/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.filip.dajsve", appContext.getPackageName());
+        assertEquals("hr.foi.air.dajsve", appContext.getPackageName());
     }
 }
